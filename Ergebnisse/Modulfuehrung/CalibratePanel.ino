abe6053768511a02e502796d9fffa63bcb84b171
lJobot88l <johannewaldsen@gmail.com>
2022-12-14 14:36:52 +0100

    Ende Aufbereitung von Quellendokumentation, beginn arduinodokumentation

--- Ergebnisse/Modulfuehrung/CalibratePanel.ino
+++ Ergebnisse/Modulfuehrung/CalibratePanel.ino
@@ -1,13 +1,4 @@
 void Calibrate()
 {
-    digitalWrite(turnL, HIGH);
-    while(true) 
-    {
-        if(digitalRead(leftSideHitS))
-        {
-            delay(50); // debounce
-            if(digitalRead(leftSideHitS)) { break; }
-        }            
-    }
-    digitalWrite(turnL, LOW);
+
 } 
--- Ergebnisse/Modulfuehrung/Modulfuehrung.ino
+++ Ergebnisse/Modulfuehrung/Modulfuehrung.ino
@@ -16,18 +16,22 @@ int autoTurn = true; // 1 = autoturn engaged
 
 int loopbegin = 0;
 
-int dLenS = 864; // 86400 = 1 day
-int rTimeS = 48;
-int sTimeS = dLenS/4;
-int intervals = 8;
-int intervalTimeS = rTimeS/intervals;
-float opsFactor = (float)rTimeS/(float)sTimeS;
-int modS = intervalTimeS/opsFactor;
+unsigned long startRotation; // Has to be ulong because int would overflow after ~2 intervals, now overflows after 50 days (like millis())
+
+// All timespans in seconds
+int dLenS = 86400; // 86400 = 1 day
+int rTimeS = 45; // Time taken for a full rotation
+int sTimeS = dLenS/4; // Fraction of the day that has sun (1/4 of the full day)
+int intervals = 8; // Times the panel rotates
+int intervalTimeS = rTimeS/intervals; // The length of one rotation interval
+float opsFactor = (float)rTimeS/(float)sTimeS; // Ratio from time taken for a rotation to the hour time. Used to convert between actual time and the equivalent rotating interval time
+int modS = intervalTimeS/opsFactor; // Length of the break between the starts of two intervals
 void setup() 
 {
 
     Serial.begin(9600);
     Serial.println("Hello, World!");
+    Serial.print("Interval time is "); Serial.print(modS); Serial.println(" seconds");
     pinMode(turnL, OUTPUT);
     pinMode(turnR, OUTPUT);
 
@@ -36,22 +40,33 @@ void setup()
 
     pinMode(turnLbutton, INPUT_PULLUP);
     pinMode(turnRbutton, INPUT_PULLUP);
-    Calibrate();
+    
+    digitalWrite(turnL, HIGH); // Turn the panel all the way left
+    while(true) 
+    {
+        if(digitalRead(leftSideHitS))
+        {
+            delay(50); // debounce
+            if(digitalRead(leftSideHitS)) { break; }
+        }            
+    }
+    digitalWrite(turnL, LOW);
     loopbegin = millis();
 }
 
 void loop() 
 {
-    if(( millis() - loopbegin <= 500 || ((millis()/1000) % modS >= -1 && (millis()/1000) % modS <= 1)) && (!digitalRead(rightSideHitS)) && autoTurn)
+    if( (( millis() - loopbegin <= 500 || ((millis() - loopbegin)/1000) % modS <= 1)) && (!digitalRead(rightSideHitS)) && autoTurn)
     {
         delay(50); // debounce
         if(!digitalRead(rightSideHitS))
         {
             digitalWrite(turnR, HIGH);
-            int startR = millis();
-            Serial.println("Autorotating...");
-            while(((millis() - startR)/1000) < intervalTimeS)
+            startRotation = millis();
+            Serial.println("Autorotating..."); // Rotates automatically every modS seconds for intervalTimeS seconds
+            while(((millis() - startRotation)/1000) < intervalTimeS)
             {
+
                 if(digitalRead(rightSideHitS))
                 {
                     delay(50); // debounce
@@ -60,6 +75,7 @@ void loop()
                         autoTurn = false;
                         digitalWrite(turnR, LOW); // disables auto turn when hit
                         Serial.println("Autorotation cancelled (sensor hit)");
+                        Serial.println("Sleeping..."); // Sleeps until the end of the day when the sensor was hit
                         return;
                     }
                 }
@@ -68,8 +84,9 @@ void loop()
             Serial.println("Autorotation finished!");
         }
     }
-    if((millis()/1000) % (dLenS-50) == 0)
+    if((millis()/1000) % (dLenS-50) == 0) // Resets the rotation after a full day
     {
+        Serial.println("Waking...");
         digitalWrite(turnL, HIGH);
         Serial.println("Resetting autorotation...");
         Serial.println((millis()/1000) % (dLenS-50));
@@ -86,13 +103,13 @@ void loop()
             if((millis()/1000 % dLenS) == 0)
             {
                 autoTurn = true;
-                Serial.println("Autorotation reset!");
+                Serial.println("Autorotation reset!"); // Turns autoturn back on once the day begins anew
                 break;
             }
         }
 
     }
-    if(!digitalRead(leftSideHitS) && !digitalRead(turnLbutton) && !bLPressed)
+    if(!digitalRead(leftSideHitS) && !digitalRead(turnLbutton) && !bLPressed) // Following 4 if blocks are for using buttons
     {
         delay(50);
         if(!digitalRead(turnLbutton))
@@ -134,26 +151,3 @@ void loop()
         }
     }
 }
-
-    // --- Knopfbetrieb: ---
-    // if(!digitalRead(leftSideHitS) && !digitalRead(turnLbutton) && !bLPressed)
-    // {
-    //     digitalWrite(turnL, HIGH);
-    //     bLPressed = true;
-    // }
-    // if(!digitalRead(rightSideHitS) && !digitalRead(turnRbutton) && !bRPressed)
-    // {
-    //     digitalWrite(turnR, HIGH);
-    //     bRPressed = true;
-    // }
-
-    // if(bLPressed && (digitalRead(turnLbutton) || digitalRead(leftSideHitS)))
-    // {
-    //     digitalWrite(turnL, LOW);
-    //     bLPressed = false;
-    // }
-    // if(bRPressed && (digitalRead(turnRbutton) || digitalRead(rightSideHitS)))
-    // {
-    //     digitalWrite(turnR, LOW);
-    //     bRPressed = false;
-    // }
